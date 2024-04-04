beafbdd94387a624001461eb891bf7fddf089dfe
Daulet <lepessov.daulet@gmail.com>
2024-04-04 19:29:23 +0500

    Added task migrations and etc

---
+++ todo-list/app/Http/Controllers/TaskController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\CreateTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    public function store(CreateTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+        return response()->json($task, 201);
+    }
+
+    public function show($id)
+    {
+        $task = Task::findOrFail($id);
+        return response()->json($task);
+    }
+
+    public function update(UpdateTaskRequest $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $task->update($request->validated());
+        return response()->json($task);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
---
+++ todo-list/app/Http/Requests/CreateTaskRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTaskRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'nullable|in:todo,in_progress,done',
+            'due_date' => 'nullable|date',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title field is required.',
+            'title.string' => 'The title must be a string.',
+            'title.max' => 'The title may not be greater than :max characters.',
+            'description.string' => 'The description must be a string.',
+            'status.in' => 'The status must be one of: todo, in_progress, done.',
+            'due_date.date' => 'The due date must be a valid date.',
+        ];
+    }
+}
