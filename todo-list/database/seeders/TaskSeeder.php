4fee1bb0064f3ce10e835b915b1f05c9386266c9
Daulet <lepessov.daulet@gmail.com>
2024-04-05 08:00:22 +0500

    Added task model migration and etc

---
+++ todo-list/database/seeders/TaskSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Task;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Task::create([
+            'title' => 'Task 1',
+            'description' => 'Description for Task 1',
+            'status' => 'todo',
+        ]);
+
+        Task::create([
+            'title' => 'Task 2',
+            'description' => 'Description for Task 2',
+            'status' => 'in_progress',
+        ]);
+
+        Task::create([
+            'title' => 'Task 3',
+            'description' => 'Description for Task 3',
+            'status' => 'done',
+        ]);
+    }
+}
